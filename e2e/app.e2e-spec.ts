f2a68884ee4487d138684ce1b97ac0fbe64d5049
angular-cli <angular-cli@angular.io>
2016-09-03 11:24:47 +0200

    chore: initial commit from angular-cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { BaylerCliAngularPage } from './app.po';
+
+describe('bayler-cli-angular App', function() {
+  let page: BaylerCliAngularPage;
+
+  beforeEach(() => {
+    page = new BaylerCliAngularPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
---
+++ e2e/app.po.ts
@@ -0,0 +1,9 @@
+export class BaylerCliAngularPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
